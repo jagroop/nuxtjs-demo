f613e1adeeb9fd0d40e800e4b6f5ca6d6e812eb8
Jagroop Singh <geek.jagroop@gmail.com>
2017-08-07 12:29:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Nuxt.JS Demo app
+
+## Installation
+
+```
+git clone https://github.com/jagroop/nuxtjs-demo.git
+cd nuxtjs-demo
+npm install # yarn install
+npm run dev
+```
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+        <div class="nav">
+          <ul>
+            <li><nuxt-link  to="/">Home</nuxt-link></li>
+            <li><nuxt-link  to="/about">About</nuxt-link></li>
+            <li><nuxt-link  to="/work">Work</nuxt-link></li>
+          </ul>
+        </div>
+    </header>
+    <div class="container">    
+      <nuxt></nuxt>
+    </div> 
+  </div> 
+</template>
